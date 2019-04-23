d14283f9cd411e0f707e546b0c69796eae30f0c6
omranhaidari <omran.haidari@etu.univ-lyon1.fr>
2019-04-24 00:04:34 +0200

    Client fonctionnelle avec interface

---
+++ client/src/client/GUI.java
@@ -0,0 +1,160 @@
+package client;
+
+import client.states.ConnectionState;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.util.List;
+
+public class GUI extends javax.swing.JFrame implements Observer, ActionListener {
+
+    private JTextField textFieldAddress;
+    private JTextField textFieldPort;
+    private JTextField textFieldUsername;
+    private JTextField textFieldFrom;
+    private JTextField textFieldTo;
+    private JTextField textFieldSubject;
+    private JButton buttonSendEmail;
+    private JTextArea textAreaContent;
+    private JButton buttonCancel;
+
+    private Stack<Client> clients;
+
+    public GUI() {
+        JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+        JPanel panel = new JPanel();
+        panel.add(splitPane);
+
+        JPanel panelSplitTop = new JPanel(new BorderLayout());
+
+        JPanel panelTopFields = new JPanel(new GridLayout(0,2));
+        JLabel labelAddress = new JLabel("Address :");
+        this.textFieldAddress = new JTextField("127.0.0.1");
+        JLabel labelPort = new JLabel("Port :");
+        this.textFieldPort = new JTextField("1337");
+        JLabel labelUsername = new JLabel("User name :");
+        this.textFieldUsername = new JTextField("John");
+
+        panelTopFields.add(labelAddress);
+        panelTopFields.add(this.textFieldAddress);
+        panelTopFields.add(labelPort);
+        panelTopFields.add(this.textFieldPort);
+        panelTopFields.add(labelUsername);
+        panelTopFields.add(this.textFieldUsername);
+
+        panelSplitTop.add(panelTopFields, BorderLayout.CENTER);
+        //splitPane.setTopComponent(panelSplitTop);
+
+        JPanel panelSplitBottom = new JPanel(new BorderLayout());
+
+        JPanel panelMessageAndHeader = new JPanel(new BorderLayout());
+        JPanel panelHeader = new JPanel(new BorderLayout());
+        JPanel panelHeaderCenter = new JPanel(new GridLayout(0,1));
+        this.textFieldFrom = new JTextField("omran@gmail.com");
+        panelHeaderCenter.add(this.textFieldFrom);
+        this.textFieldTo = new JTextField("Jane@polytech.com, John@polytech.com, Martin@polytech.com, Doe@informatique.fr, informatique@quatra.polytech");
+        panelHeaderCenter.add(this.textFieldTo);
+        this.textFieldSubject = new JTextField("Information importante");
+        panelHeaderCenter.add(this.textFieldSubject);
+        this.textAreaContent = new JTextArea("Bonjour !\n\nLa réunion aura lieu ce mercredi à 08h30. Merci de confirmer votre présence.\n\nCordialement, Omran.");
+
+        JPanel panelHeaderLeft = new JPanel(new GridLayout(0,1));
+        panelHeaderLeft.add(new JLabel("From : "));
+        panelHeaderLeft.add(new JLabel("To : "));
+        panelHeaderLeft.add(new JLabel("Subject : "));
+
+        this.buttonCancel = new JButton("Annuler");
+        this.buttonCancel.addActionListener(this);
+
+        panelHeader.add(panelHeaderLeft, BorderLayout.WEST);
+        panelHeader.add(panelHeaderCenter, BorderLayout.CENTER);
+        panelHeader.add(this.buttonCancel, BorderLayout.EAST);
+
+        panelMessageAndHeader.add(panelHeader, BorderLayout.NORTH);
+        panelMessageAndHeader.add(this.textAreaContent, BorderLayout.CENTER);
+
+        this.buttonSendEmail = new JButton("Envoyer");
+
+        panelSplitBottom.add(panelMessageAndHeader, BorderLayout.CENTER);
+        panelSplitBottom.add(this.buttonSendEmail, BorderLayout.SOUTH);
+        splitPane.setBottomComponent(panelSplitBottom);
+
+        setContentPane(splitPane);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(700, 400);
+        setVisible(true);
+
+        this.buttonSendEmail.addActionListener(this);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+
+
+    private void clearFields() {
+        this.textFieldSubject.setText("");
+        this.textFieldTo.setText("");
+        this.textFieldFrom.setText("");
+        this.textAreaContent.setText("");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == this.buttonSendEmail) {
+
+            Message message = getMessageFromFields();
+
+            this.clients = new Stack<>();
+            for (String s : getRecipients(this.textFieldTo.getText())) {
+                if (!s.isEmpty()) {
+                    message.addRecipient(s);
+                }
+            }
+
+            for (String s : message.getAllDomains()) {
+                int port = PortFactory.getPort(s);
+                String address = AddressFactory.getAddress(s);
+                if (!address.isEmpty() && port != -1) {
+                    Client client = new Client(address, port, s);
+                    client.addObserver(this);
+                    client.setMessage(message);
+                    client.setState(new ConnectionState(client));
+                    this.clients.add(client);
+                }
+            }
+
+
+            message.generateBody();
+            this.connectToNextClient();
+
+        } else if (e.getSource() == this.buttonCancel) {
+            this.clearFields();
+        }
+    }
+
+    private boolean connectToNextClient() {
+        if (!this.clients.empty()) {
+            this.clients.pop().connect();
+            return true;
+        }
+        return false;
+    }
+
+    private Message getMessageFromFields() {
+        Message message = new Message();
+        message.setFrom(this.textFieldFrom.getText());
+        message.setSubject(this.textFieldSubject.getText());
+        message.setBody(this.textAreaContent.getText());
+        return message;
+    }
+
+    public List<String> getRecipients(String fromLine) {
+        return Arrays.asList(fromLine.replace(" ", "").split(","));
+    }
+}
