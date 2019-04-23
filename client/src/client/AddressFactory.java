db6c659395aad4947fa13f21a6cdaf3966db5311
pierreelliott <pierre-elliott.thiboud@etu.univ-lyon1.fr>
2019-04-24 00:27:26 +0200

    Passage des @ sur localhost par défaut
    Plus pratique pour les tests ;-)

--- client/src/client/AddressFactory.java
+++ client/src/client/AddressFactory.java
@@ -3,11 +3,11 @@ package client;
 public class AddressFactory {
     public static String getAddress(String domain) {
         if (domain.equals("quatrea.uk")) {
-            return ""; // adresse IP du client
+            return "127.0.0.1"; // adresse IP du client
         } else if (domain.equals("informatique.fr")) {
-            return ""; // adresse IP du client
+            return "127.0.0.1"; // adresse IP du client
         } else if (domain.equals("polytech.com")) {
-            return ""; // adresse IP du client
+            return "127.0.0.1"; // adresse IP du client
         }
         return "";
     }
